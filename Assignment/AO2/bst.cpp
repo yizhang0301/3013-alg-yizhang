07b2ed2a7991825aa7bc0c46c93c82aa46a86605
yizhang0301 <43011146+yizhang0301@users.noreply.github.com>
2019-09-19 03:29:42 +0800

    Update bst.cpp

--- Assignment/AO2/bst.cpp
+++ Assignment/AO2/bst.cpp
@@ -1,13 +1,10 @@
-/******************************************************************************
-Name: YIZHANG
-Date: 2019/9/19
-Class: CMPS 3013
-This program  reads  value insert them into a tree until it full
-/******************************************************************************
+
+
+
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <vector>
 
 
 
@@ -15,285 +12,285 @@ using namespace std;
 
 struct node
 {
-    int data;
-    node *left;
-    node *right;
-    node()
-    {
-        data = -1;
-        left = NULL;
-        right = NULL;
-    }
-    node(int x)
-    {
-        data = x;
-        left = NULL;
-        right = NULL;
-    }
+	int data;
+	node *left;
+	node *right;
+	node()
+	{
+		data = -1;
+		left = NULL;
+		right = NULL;
+	}
+	node(int x)
+	{
+		data = x;
+		left = NULL;
+		right = NULL;
+	}
 };
 
 class BSTree
 {
 private:
-    node *root;
+	node *root;
 
-    int count(node *root)
-    {
-        if (!root)
-        {
-            return 0;
-        }
-        else
-        {
-            return 1 + count(root->left) + count(root->right);
-        }
-    }
+	int count(node *root)
+	{
+		if (!root)
+		{
+			return 0;
+		}
+		else
+		{
+			return 1 + count(root->left) + count(root->right);
+		}
+	}
 
-    void insert(node *&root, node *&temp)
-    {
-        if (!root)
-        {
-            root = temp;
-        }
-        else
-        {
-            if (temp->data < root->data)
-            {
-                insert(root->left, temp);
-            }
-            else
-            {
-                insert(root->right, temp);
-            }
-        }
-    }
+	void insert(node *&root, node *&temp)
+	{
+		if (!root)
+		{
+			root = temp;
+		}
+		else
+		{
+			if (temp->data < root->data)
+			{
+				insert(root->left, temp);
+			}
+			else
+			{
+				insert(root->right, temp);
+			}
+		}
+	}
 
-    void print_node(node *n, string label = "")
-    {
-        if (label != "")
-        {
-            cout << "[" << label << "]";
-        }
-        cout << "[[" << n << "][" << n->data << "]]\n";
-        if (n->left)
-        {
-            cout << "\t|-->[L][[" << n->left << "][" << n->left->data << "]]\n";
-        }
-        else
-        {
-            cout << "\t\\-->[L][null]\n";
-        }
-        if (n->right)
-        {
-            cout << "\t\\-->[R][[" << n->right << "][" << n->right->data << "]]\n";
-        }
-        else
-        {
-            cout << "\t\\-->[R][null]\n";
-        }
-    }
+	void print_node(node *n, string label = "")
+	{
+		if (label != "")
+		{
+			cout << "[" << label << "]";
+		}
+		cout << "[[" << n << "][" << n->data << "]]\n";
+		if (n->left)
+		{
+			cout << "\t|-->[L][[" << n->left << "][" << n->left->data << "]]\n";
+		}
+		else
+		{
+			cout << "\t\\-->[L][null]\n";
+		}
+		if (n->right)
+		{
+			cout << "\t\\-->[R][[" << n->right << "][" << n->right->data << "]]\n";
+		}
+		else
+		{
+			cout << "\t\\-->[R][null]\n";
+		}
+	}
 
-    /**
-     * type = ['predecessor','successor']
-     */
-    node *minValueNode(node *root)
-    {
-        node *current = root;
+	/**
+	 * type = ['predecessor','successor']
+	 */
+node* minValueNode(node* root)
+{
+	node* current = root;
 
-        if (root->right)
-        {
-            current = root->right;
-            while (current->left != NULL)
-            {
-                current = current->left;
-            }
-        }
-        else if (root->left)
-        {
-            current = root->left;
-            while (current->right != NULL)
-            {
-                current = current->right;
-            }
-        }
+	if (root->right)
+	{
+		current = root->right;
+		while (current->left != NULL)
+		{
+			current = current->left;
+		}
+	}
+	else if (root->left)
+	{
+		current = root->left;
+		while (current->right != NULL)
+		{
+			current = current->right;
+		}
+	}
 
-        return current;
-    }
+	return current;
+}
 
-    int height(node *root)
-    {
-        if (!root)
-        {
-            return 0;
-        }
-        else
-        {
-            int left = height(root->left);
-            int right = height(root->right);
-            if (left > right)
-            {
-                return left + 1;
-            }
-            else
-            {
-                return right + 1;
-            }
-        }
-    }
+int height(node* root)
+{
+	if (!root)
+	{
+		return 0;
+	}
+	else
+	{
+		int left = height(root->left);
+		int right = height(root->right);
+		if (left > right)
+		{
+			return left + 1;
+		}
+		else
+		{
+			return right + 1;
+		}
+	}
+}
 
-    /* Print nodes at a given level */
-    void printGivenLevel(node *root, int level)
-    {
-        if (root == NULL)
-            return;
-        if (level == 1)
-        {
-            print_node(root);
-        }
-        else if (level > 1)
-        {
-            printGivenLevel(root->left, level - 1);
-            printGivenLevel(root->right, level - 1);
-        }
-    }
-    //************************************************************************
-    // Method to help create GraphViz code so the expression tree can
-    // be visualized. This method prints out all the unique node id's
-    // by traversing the tree.
-    // Recivies a node pointer to root and performs a simple recursive
-    // tree traversal.
-    //************************************************************************
-    void GraphVizGetIds(node *nodePtr, ofstream &VizOut)
-    {
-        static int NullCount = 0;
-        if (nodePtr)
-        {
-            GraphVizGetIds(nodePtr->left, VizOut);
-            VizOut << "node" << nodePtr->data
-                   << "[label=\"" << nodePtr->data << "\\n"
-                   //<<"Add:"<<nodePtr<<"\\n"
-                   //<<"Par:"<<nodePtr->parent<<"\\n"
-                   //<<"Rt:"<<nodePtr->right<<"\\n"
-                   //<<"Lt:"<<nodePtr->left<<"\\n"
-                   << "\"]" << endl;
-            if (!nodePtr->left)
-            {
-                NullCount++;
-                VizOut << "nnode" << NullCount << "[label=\"X\",shape=point,width=.15]" << endl;
-            }
-            GraphVizGetIds(nodePtr->right, VizOut);
-            if (!nodePtr->right)
-            {
-                NullCount++;
-                VizOut << "nnode" << NullCount << "[label=\"X\",shape=point,width=.15]" << endl;
-            }
-        }
-    }
+/* Print nodes at a given level */
+void printGivenLevel(node* root, int level)
+{
+	if (root == NULL)
+		return;
+	if (level == 1)
+	{
+		print_node(root);
+	}
+	else if (level > 1)
+	{
+		printGivenLevel(root->left, level - 1);
+		printGivenLevel(root->right, level - 1);
+	}
+}
+//************************************************************************
+// Method to help create GraphViz code so the expression tree can
+// be visualized. This method prints out all the unique node id's
+// by traversing the tree.
+// Recivies a node pointer to root and performs a simple recursive
+// tree traversal.
+//************************************************************************
+void GraphVizGetIds(node* nodePtr, ofstream& VizOut)
+{
+	static int NullCount = 0;
+	if (nodePtr)
+	{
+		GraphVizGetIds(nodePtr->left, VizOut);
+		VizOut << "node" << nodePtr->data
+			<< "[label=\"" << nodePtr->data << "\\n"
+			//<<"Add:"<<nodePtr<<"\\n"
+			//<<"Par:"<<nodePtr->parent<<"\\n"
+			//<<"Rt:"<<nodePtr->right<<"\\n"
+			//<<"Lt:"<<nodePtr->left<<"\\n"
+			<< "\"]" << endl;
+		if (!nodePtr->left)
+		{
+			NullCount++;
+			VizOut << "nnode" << NullCount << "[label=\"X\",shape=point,width=.15]" << endl;
+		}
+		GraphVizGetIds(nodePtr->right, VizOut);
+		if (!nodePtr->right)
+		{
+			NullCount++;
+			VizOut << "nnode" << NullCount << "[label=\"X\",shape=point,width=.15]" << endl;
+		}
+	}
+}
 
-    //************************************************************************
-    // This method is partnered with the above method, but on this pass it
-    // writes out the actual data from each node.
-    // Don't worry about what this method and the above method do, just
-    // use the output as your told:)
-    //************************************************************************
-    void GraphVizMakeConnections(node *nodePtr, ofstream &VizOut)
-    {
-        static int NullCount = 0;
-        if (nodePtr)
-        {
-            GraphVizMakeConnections(nodePtr->left, VizOut);
-            if (nodePtr->left)
-                VizOut << "node" << nodePtr->data << "->"
-                       << "node" << nodePtr->left->data << endl;
-            else
-            {
-                NullCount++;
-                VizOut << "node" << nodePtr->data << "->"
-                       << "nnode" << NullCount << endl;
-            }
+//************************************************************************
+// This method is partnered with the above method, but on this pass it
+// writes out the actual data from each node.
+// Don't worry about what this method and the above method do, just
+// use the output as your told:)
+//************************************************************************
+void GraphVizMakeConnections(node* nodePtr, ofstream& VizOut)
+{
+	static int NullCount = 0;
+	if (nodePtr)
+	{
+		GraphVizMakeConnections(nodePtr->left, VizOut);
+		if (nodePtr->left)
+			VizOut << "node" << nodePtr->data << "->"
+			<< "node" << nodePtr->left->data << endl;
+		else
+		{
+			NullCount++;
+			VizOut << "node" << nodePtr->data << "->"
+				<< "nnode" << NullCount << endl;
+		}
 
-            if (nodePtr->right)
-                VizOut << "node" << nodePtr->data << "->"
-                       << "node" << nodePtr->right->data << endl;
-            else
-            {
-                NullCount++;
-                VizOut << "node" << nodePtr->data << "->"
-                       << "nnode" << NullCount << endl;
-            }
+		if (nodePtr->right)
+			VizOut << "node" << nodePtr->data << "->"
+			<< "node" << nodePtr->right->data << endl;
+		else
+		{
+			NullCount++;
+			VizOut << "node" << nodePtr->data << "->"
+				<< "nnode" << NullCount << endl;
+		}
 
-            GraphVizMakeConnections(nodePtr->right, VizOut);
-        }
-    }
+		GraphVizMakeConnections(nodePtr->right, VizOut);
+	}
+}
 
 public:
-    BSTree()
-    {
-        root = NULL;
-    }
-    ~BSTree()
-    {
-    }
+	BSTree()
+	{
+		root = NULL;
+	}
+	~BSTree()
+	{
+	}
 
-    int count()
-    {
-        return count(root);
-    }
+	int count()
+	{
+		return count(root);
+	}
 
-    void insert(int x)
-    {
-        node *temp = new node(x);
-        insert(root, temp);
-    }
+	void insert(int x)
+	{
+		node* temp = new node(x);
+		insert(root, temp);
+	}
 
-    void minValue()
-    {
-        print_node(minValueNode(root), "minVal");
-    }
+	void minValue()
+	{
+		print_node(minValueNode(root), "minVal");
+	}
 
-    int height()
-    {
+	int height()
+	{
 
-        return height(root);
-    }
+		return height(root);
+	}
 
-    int top()
-    {
-        if (root)
-            return root->data;
-        else
-            return 0;
-    }
+	int top()
+	{
+		if (root)
+			return root->data;
+		else
+			return 0;
+	}
 
-    /* Function to line by line print level order traversal a tree*/
-    void printLevelOrder()
-    {
-        cout << "Begin Level Order===================\n";
-        int h = height(root);
-        int i;
-        for (i = 1; i <= h; i++)
-        {
-            printGivenLevel(root, i);
-            cout << "\n";
-        }
-        cout << "End Level Order===================\n";
-    }
+	/* Function to line by line print level order traversal a tree*/
+	void printLevelOrder()
+	{
+		cout << "Begin Level Order===================\n";
+		int h = height(root);
+		int i;
+		for (i = 1; i <= h; i++)
+		{
+			printGivenLevel(root, i);
+			cout << "\n";
+		}
+		cout << "End Level Order===================\n";
+	}
 
-    //************************************************************************
-    // Recieves a filename to place the GraphViz data into.
-    // It then calls the above two graphviz methods to create a data file
-    // that can be used to visualize your expression tree.
-    //************************************************************************
-    void GraphVizOut(string filename)
-    {
-        ofstream VizOut;
-        VizOut.open(filename);
-        VizOut << "Digraph G {\n";
-        GraphVizGetIds(root, VizOut);
-        GraphVizMakeConnections(root, VizOut);
-        VizOut << "}\n";
-        VizOut.close();
-    }
+	//************************************************************************
+	// Recieves a filename to place the GraphViz data into.
+	// It then calls the above two graphviz methods to create a data file
+	// that can be used to visualize your expression tree.
+	//************************************************************************
+	void GraphVizOut(string filename)
+	{
+		ofstream VizOut;
+		VizOut.open(filename);
+		VizOut << "Digraph G {\n";
+		GraphVizGetIds(root, VizOut);
+		GraphVizMakeConnections(root, VizOut);
+		VizOut << "}\n";
+		VizOut.close();
+	}
 };
 void openFiles(ifstream& infile, ofstream& outfile)
 {
@@ -301,7 +298,7 @@ void openFiles(ifstream& infile, ofstream& outfile)
 	cout << "enter the name of input file:";
 	cin >> infileName;
 	infile.open(infileName);
-	
+
 
 }
 
@@ -313,14 +310,28 @@ int main()
 	openFiles(infile, outfile);
 	BSTree B;
 	vector<int> a;
-  int num;
-  while(!infile.eof()){
-    infile >> num;
-     a.push_back(num);
-  }
-  printVector(a);
-  bubbleSort(a);
-  printVector(a);
-  B.sortedvectorToBST(s);
-	B.printLevelOrder();
+	int num;
+	while (!infile.eof()) {
+		infile >> num;
+		a.push_back(num);
+	}
+	
+	bool check = true;
+	while (check) {
+		check = false;
+		for (size_t i = 0; i < a.size() - 1; i++) {
+			if (a[i] > a[i + 1]) {
+				a[i] += a[i + 1];
+				a[i + 1] = a[i] - a[i + 1];
+				a[i] -= a[i + 1];
+				check = true;
+			}
+		}
+		for (size_t i = 0; i < a.size(); i++) {
+			cout << a[i] << " ";
+
+		}
+		cout << endl;
+		return 0;
+	}
 }
