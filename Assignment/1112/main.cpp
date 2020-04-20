dcdc6aac0012cdd161bdfc112a8b1221975a3b85
yizhang0301 <43011146+yizhang0301@users.noreply.github.com>
2020-04-21 03:37:08 +0800

    Create main.cpp

---
+++ Assignment/1112/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <fstream>
+#include <cstring>
+ 
+using namespace std;
+ 
+const int N = 110;
+
+int main()
+{
+	int cases;
+ 
